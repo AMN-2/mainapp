9194d621e1447d9327907e3ca51c1b332c57acc9
ameenali <mqtdyh122@gmail.com>
2023-10-16 13:59:28 -1200

    UPDATE : update the app to the 0.1v

--- lib/main.dart
+++ lib/main.dart
@@ -1,146 +1,61 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, use_key_in_widget_constructors, must_be_immutable, avoid_unnecessary_containers
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  List names = [
+    {"name": "ameen", "age": "12"},
+    {"name": "ahmed", "age": "15"},
+    {"name": "ali", "age": "17"},
+    {"name": "abbass", "age": "20"},
+    {"name": "kadhim", "age": "30"},
+    {"name": "ameen", "age": "12"},
+    {"name": "ahmed", "age": "15"},
+    {"name": "ali", "age": "17"},
+    {"name": "abbass", "age": "20"},
+    {"name": "kadhim", "age": "30"},
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(),
-          body: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 96, 92, 78),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 10, 8, 2),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Color.fromARGB(255, 122, 92, 1),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 96, 92, 78),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 10, 8, 2),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Color.fromARGB(255, 122, 92, 1),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 96, 92, 78),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 10, 8, 2),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Color.fromARGB(255, 122, 92, 1),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 96, 92, 78),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 10, 8, 2),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Color.fromARGB(255, 122, 92, 1),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 96, 92, 78),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 10, 8, 2),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Color.fromARGB(255, 122, 92, 1),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Colors.amber,
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 96, 92, 78),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: const Color.fromARGB(255, 10, 8, 2),
-                ),
-                Container(
-                  height: 100,
-                  width: 100,
-                  color: Color.fromARGB(255, 122, 92, 1),
-                ),
-              ],
-            ),
-          )),
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: Center(
+            child: Text("First App "),
+          ),
+        ),
+        body: ListView.separated(
+          separatorBuilder: (context, i) {
+            return Container(
+              color: Color.fromARGB(221, 124, 88, 88),
+            );
+          },
+          itemCount: names.length,
+          itemBuilder: (context, i) {
+            return Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.only(left: 10.7),
+              width: 100,
+              height: 100,
+              color: i.isEven
+                  ? Color.fromARGB(255, 255, 255, 255)
+                  : Colors.blueGrey,
+              child: Text(
+                "$i" + " - " + names[i]["name"] + " =>",
+                style: TextStyle(
+                    color: i.isEven ? Colors.black : Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
